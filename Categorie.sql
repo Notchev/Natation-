c68c24c620193f36082447a8eef0c6fadfbfeb11
Anne-Lise Hervé <anne-liseherve@MacBook-Air-de-Anne-Lise.local>
2019-04-30 11:23:39 +0200

    cat bdd

---
+++ Categorie.sql
@@ -0,0 +1,6 @@
+
+
+Insert into Categorie (ID, Nom, IDBestSeller, Photo) values (1,"Livre",1, "livres.jpeg");
+Insert into Categorie (ID, Nom, IDBestSeller, Photo) values (2,"Musique",6, "musique.jpeg");
+Insert into Categorie (ID, Nom, IDBestSeller, Photo) values (3,"Sports&Loisirs",13, "sports.jpeg");
+Insert into Categorie (ID, Nom, IDBestSeller, Photo) values (4,"Vetements",19, "vetements.jpeg");
