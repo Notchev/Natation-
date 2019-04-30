328fe3ce9adf66032b8bad0fa1a7039407dc01bb
Sarah Kardache <sarahkardache@MacBook-Pro-de-Sarah.local>
2019-04-30 10:51:37 +0200

    Bdd des articles

---
+++ Article.sql
@@ -0,0 +1,158 @@
+
+Insert into Article (ID,Photo,IDType) values (1,"hungergames.jpeg",1);
+Insert into Article (ID,Photo,IDType) values (2,"hungergames.jpeg",1);
+Insert into Article (ID,Photo,IDType) values (3,"hungergames.jpeg",1);
+Insert into Article (ID,Photo,IDType) values (4,"hungergames.jpeg",1);
+Insert into Article (ID,Photo,IDType) values (5,"hungergames.jpeg",1);
+
+Insert into Article (ID,Photo,IDType) values (6,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (7,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (8,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (9,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (10,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (11,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (12,"hautdehurlevent.jpg",2);
+Insert into Article (ID,Photo,IDType) values (13,"hautdehurlevent.jpg",2);
+
+Insert into Article (ID,Photo,IDType) values (14,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (15,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (16,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (17,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (18,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (19,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (20,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (21,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (22,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (23,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (24,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (85,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (86,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (87,"harrypotter.jpeg",3);
+Insert into Article (ID,Photo,IDType) values (88,"harrypotter.jpeg",3);
+
+Insert into Article (ID,Photo,IDType) values (89,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (90,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (91,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (92,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (93,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (94,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (95,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (96,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (97,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (98,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (99,"orgueiletprejuges.jpeg",4);
+Insert into Article (ID,Photo,IDType) values (100,"orgueiletprejuges.jpeg",4);
+
+Insert into Article (ID,Photo,IDType) values (25,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (26,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (27,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (28,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (29,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (30,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (31,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (32,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (33,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (34,"FranceGall.jpeg",5);
+Insert into Article (ID,Photo,IDType) values (35,"FranceGall.jpeg",5);
+
+Insert into Article (ID,Photo,IDType) values (36,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (37,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (38,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (39,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (40,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (41,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (42,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (43,"Gainsbourg.jpeg",6);
+Insert into Article (ID,Photo,IDType) values (44,"Gainsbourg.jpeg",6);
+
+
+Insert into Article (ID,Photo,IDType) values (45,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (46,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (47,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (48,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (49,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (50,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (51,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (52,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (53,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (54,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (55,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (56,"Johnny.jpeg",7);
+Insert into Article (ID,Photo,IDType) values (57,"Johnny.jpeg",7);
+
+
+Insert into Article (ID,Photo,IDType) values (58,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (59,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (60,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (61,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (62,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (63,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (64,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (65,"Angele.jpeg",8);
+Insert into Article (ID,Photo,IDType) values (66,"Angele.jpeg",8);
+
+
+Insert into Article (ID,Photo,IDType) values (67,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (68,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (69,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (70,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (71,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (72,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (73,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (74,"Stromae.jpeg",9);
+Insert into Article (ID,Photo,IDType) values (75,"Stromae.jpeg",9);
+
+
+Insert into Article (ID,Photo,IDType) values (76,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (77,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (78,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (79,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (80,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (81,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (82,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (83,"Orelsan.jpeg",10);
+Insert into Article (ID,Photo,IDType) values (84,"Orelsan.jpeg",10);
+
+
+Insert into Article (ID,Photo,IDType) values (101,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (102,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (103,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (104,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (105,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (106,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (107,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (108,"PingPong.jpeg",11);
+Insert into Article (ID,Photo,IDType) values (109,"PingPong.jpeg",11);
+
+Insert into Article (ID,Photo,IDType) values (110,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (111,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (112,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (113,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (114,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (115,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (116,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (117,"Piscine.jpeg",12);
+Insert into Article (ID,Photo,IDType) values (118,"Piscine.jpeg",12);
+
+Insert into Article (ID,Photo,IDType) values (119,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (120,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (121,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (122,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (123,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (124,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (125,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (126,"Kimono.jpeg",13);
+Insert into Article (ID,Photo,IDType) values (127,"Kimono.jpeg",13);
+
+
+Insert into Article (ID,Photo,IDType) values (128,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (129,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (130,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (131,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (132,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (134,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (135,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (136,"Palmes.jpeg",14);
+Insert into Article (ID,Photo,IDType) values (137,"Palmes.jpeg",14);
+
+
