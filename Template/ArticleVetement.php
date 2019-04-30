6130a0b52e37290d03647174e78fe5cbe5858758
Anne-Lise Hervé <anne-liseherve@MacBook-Air-de-Anne-Lise.local>
2019-04-30 18:40:47 +0200

    update plusieurs fichiers de la part de Anne-Lise

---
+++ Template/ArticleVetement.php
@@ -0,0 +1,315 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+
+  <title>Amazon ECE</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css.css" rel="stylesheet" type="text/css"/>
+
+  <!-- Custom styles for this template -->
+  <link href="css/shop-homepage.css" rel="stylesheet">
+<script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar  navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container">
+      <a class="navbar-brand amazon1" href="#"> Amazon ECE  
+        <img src="Logo_ECE_Paris.png" width="110" height="30"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+             <div class="dropdown">
+              <button class="btn nav-link dropdown-toggle active" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Catégorie<ion-icon name="clipboard"></ion-icon>
+
+              </button>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+              <a class="dropdown-item" href="Livres.php">Livres <ion-icon name="book"></ion-icon></a>
+
+              <a class="dropdown-item" href="#">Musiques <ion-icon name="volume-high"></ion-icon></a>
+
+              <a class="dropdown-item" href="#">Sports et loisirs <ion-icon name="basketball"></ion-icon></a>
+
+              <a class="dropdown-item" href="#">Vetements <ion-icon name="woman"></ion-icon></ion-icon></a>
+
+            </div>
+          </div> 
+          </li>
+
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Top ventes<ion-icon name="bookmark"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="vendeur_login.php">Vendre<ion-icon name="briefcase"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Votre compte<ion-icon name="person"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Panier<ion-icon name="cart"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="admin_login.php">Admin<ion-icon name="school"></ion-icon></a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-3">
+        <br>
+        <h1 class="my-4">Catégories</h1>
+        <div class="list-group">
+          <br>
+           <a href="Livres.php" class="list-group-item">Livres</a>
+          <a href="Musique.php" class="list-group-item">Musique</a>
+          <a href="Sports&Loisirs.php" class="list-group-item">Sports&Loisirs</a>
+          <a href="Vetements.php" class="list-group-item active">Vêtements</a>
+        </div>
+
+      </div>
+      <!-- /.col-lg-3 -->
+
+     <div class="col-lg-9">
+
+        <br>
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            
+          </ol>
+
+
+        
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="d-block img-fluid" src="Shopping.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="Shopping.jpg" alt="Second slide">
+            </div>
+              </div>
+
+    
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+    
+      
+
+        </div>
+
+        <div class="row">
+
+   
+<?php
+ //declaration des variables
+ $IDArticlechoisi = isset ($_POST["IDArticlechoisi"])? $_POST["IDArticlechoisi"] : ""; //if-then-else (si vrai : $nom = $_POST["nom"] si faux : "") 
+ $erreur = "";
+$database = "Projet";
+//connectez-vous dans votre BDD
+//Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
+$db_handle = mysqli_connect('localhost', 'root', 'root' );
+$db_found = mysqli_select_db($db_handle, $database);
+//si le BDD existe, faire le traitement
+//on peut changer le nom de la table ici
+
+if ($db_found) {
+
+    $sql = "SELECT * FROM Type WHERE ID = $IDArticlechoisi";
+    $result = mysqli_query($db_handle, $sql);
+
+  
+    while ($data = mysqli_fetch_assoc($result)){
+
+
+ ?>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src=<?php echo $data[Photo] ?> alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#"><?php echo $data[Nom] ?></a>
+                </h4>
+                <form action="ArticleVetement.php" method="post">
+                  <table>
+                  
+                  <tr>
+                  <td> 
+                <select name= "choixpointure" value= "Sélectionner votre pointure">
+                <option value = "selectionner"> Sélectionner une pointure </option> 
+
+<?php
+ //declaration des variables
+
+$erreur = "";
+$database = "Projet";
+//connectez-vous dans votre BDD
+//Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
+$db_handle = mysqli_connect('localhost', 'root', 'root' );
+$db_found = mysqli_select_db($db_handle, $database);
+//si le BDD existe, faire le traitement
+//on peut changer le nom de la table ici
+
+if ($db_found) {
+
+    $sql = "SELECT * FROM ArticleVetement WHERE IDType = $IDArticlechoisi GROUP BY Taille";
+    $result = mysqli_query($db_handle, $sql);
+
+  
+    while ($data = mysqli_fetch_assoc($result)){ ?>
+                <option value =  <?php $data['Taille'] ?> >  <?php echo $data['Taille'] ?> </option>
+              
+
+
+ <?php   }
+    }
+//end if
+//si le BDD n'existe pas
+else {
+    echo "Database not found";
+}//end else
+//fermer la connection
+mysqli_close($db_handle);
+       ?>
+        
+
+                  </select>
+                  </td>
+                </tr>
+
+                  <tr>
+                    <td>
+                <select name= "choixcouleur" value= "Sélectionner votre année scolaire">
+                <option value = "selectionner"> Sélectionner une couleur </option> 
+
+<?php
+ //declaration des variables
+
+$erreur = "";
+$database = "Projet";
+//connectez-vous dans votre BDD
+//Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
+$db_handle = mysqli_connect('localhost', 'root', 'root' );
+$db_found = mysqli_select_db($db_handle, $database);
+//si le BDD existe, faire le traitement
+//on peut changer le nom de la table ici
+
+if ($db_found) {
+
+    $sql = "SELECT * FROM ArticleVetement WHERE IDType = $IDArticlechoisi GROUP BY Couleur";
+    $result = mysqli_query($db_handle, $sql);
+
+  
+    while ($data = mysqli_fetch_assoc($result)){ ?>
+                <option value =  <?php $data['Couleur'] ?> >  <?php echo $data['Couleur'] ?> </option>
+              
+
+
+ <?php   }
+    }
+//end if
+//si le BDD n'existe pas
+else {
+    echo "Database not found";
+}//end else
+//fermer la connection
+mysqli_close($db_handle);
+       ?>
+
+                  </select>
+                </td>
+              </tr>
+
+              <tr>
+                <td> 
+                  <input type= "radio" name= "Sexe" value="Homme" /> Homme 
+                 <input type= "radio" name= "Sexe" value="Femme" /> Femme 
+                </td>
+              </tr>
+
+              <tr>
+                  <td>
+                  <button class="btn btn-primary" type="submit"> Ajouter au Panier</button>
+                </td>
+              </tr>
+            </table>
+                </form>
+                
+
+              </div>
+            </div>
+          </div>
+
+      <?php   }
+    }
+//end if
+//si le BDD n'existe pas
+else {
+    echo "Database not found";
+}//end else
+//fermer la connection
+mysqli_close($db_handle);
+       ?>
+        
+
+
+        </div>
+        <!-- /.row -->
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ Template/vendeur_login.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+
+  <title>Amazon ECE</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css.css" rel="stylesheet" type="text/css"/>
+
+  <!-- Custom styles for this template -->
+  <link href="css/shop-homepage.css" rel="stylesheet">
+<script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar  navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container">
+      <a class="navbar-brand amazon1" href="#"> Amazon ECE  
+        <img src="Logo_ECE_Paris.png" width="90" height="20"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+             <div class="dropdown">
+              <button class="btn nav-link dropdown-toggle active" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Catégorie<ion-icon name="clipboard"></ion-icon>
+
+              </button>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+              <a class="dropdown-item" href="#">Livres <ion-icon name="book"></ion-icon></a>
+
+              <a class="dropdown-item" href="#">Musiques <ion-icon name="volume-high"></ion-icon></a>
+
+              <a class="dropdown-item" href="#">Sports et loisirs <ion-icon name="basketball"></ion-icon></a>
+
+              <a class="dropdown-item" href="#">Vetements <ion-icon name="woman"></ion-icon></ion-icon></a>
+
+            </div>
+          </div> 
+          </li>
+
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Top ventes<ion-icon name="bookmark"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Vendre<ion-icon name="briefcase"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Votre compte<ion-icon name="person"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Panier<ion-icon name="cart"></ion-icon></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Admin<ion-icon name="school"></ion-icon></a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+
+<body>
+
+  <div class="container">
+
+    <div class="row">
+      <div class="col-sm-11 col-md-9 col-lg-7 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+
+            <h6 class="card-title text-center">Se connecter</h6>
+
+
+            <form class="form-signin" action = "FormConnexionVendeur.php" method="post" >
+              <div class="form-label-group">
+                <input type="text" name="Login" class="form-control" placeholder="Login" required autofocus>
+
+
+              </div>
+
+              <div class="form-label-group">
+                <label for="inputPassword"> </label>
+
+                <input type="text" name="Mdp" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+              </div>
+
+
+              <div class="custom-control custom-checkbox mb-7">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                
+              </div>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Connexion</button>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
