dcfc58d5a17f35c6517459ff7b4aba580404a995
Sarah Kardache <sarahkardache@MacBook-Pro-de-Sarah.local>
2019-04-30 10:52:55 +0200

    BDD des vendeurs

---
+++ Vendeur.sql
@@ -0,0 +1,6 @@
+Insert into Vendeur (ID, Email, Pseudo, Mdp, Nom, Photo, Fond) values (1, "al_catraz@edu.ece.fr", "al" , "lison" , "Herve" ,  "al.jpeg", "fond_al.jpg" ); 
+Insert into Vendeur (ID, Email, Pseudo, Mdp, Nom, Photo, Fond) values (2, "sarah.kardache@edu.ece.fr", "notchev" ,"piscine", "kardache" , "sarah.jpeg", "fond_sarah.jpg" ); 
+Insert into Vendeur (ID, Email, Pseudo, Mdp, Nom, Photo, Fond) values (3, "clement@edu.ece.fr", "clem" , "lurati" ,"piscine", "clement.jpeg", "fond_clement.jpeg" ); 
+Insert into Vendeur (ID, Email, Pseudo, Mdp, Nom, Photo, Fond) values (4, "vendeurduturfu@edu.ece.fr", "turf" ,"piscine", "tuches" , "tuche.jpeg", "fond_tuche.jpg" ); 
+Insert into Vendeur (ID, Email, Pseudo, Mdp, Nom, Photo, Fond) values (5, "test@edu.ece.fr", "test" , "test" , "piscine","test.jpeg", "fond_test.jpeg" ); 
+
