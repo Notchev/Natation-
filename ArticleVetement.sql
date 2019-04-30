0c53b3ea3def48e014d292a0bf279dfab62a587f
Anne-Lise Hervé <anne-liseherve@MacBook-Air-de-Anne-Lise.local>
2019-04-30 11:23:17 +0200

    article vetement bdd

---
+++ ArticleVetement.sql
@@ -0,0 +1,52 @@
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (1, "stanverte.jpeg", 15, "Homme" , 42, "Verte"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (2, "stanverte.jpeg", 15, "Femme" , 37, "Verte"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (3, "stanverte.jpeg", 15, "Femme" , 38, "Verte"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (4, "stanverte.jpeg", 15, "Homme" , 43, "Verte"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (5, "stanverte.jpeg", 15, "Homme" , 44, "Verte"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (6, "stanrose.jpeg", 15, "Homme" , 45, "Rose"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (7, "stanrose.jpeg", 15, "Femme" , 36, "Rose"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (8, "stanrose.jpeg", 15, "Femme" , 37, "Rose"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (9, "stanrose.jpeg", 15, "Femme" , 36, "Rose"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (10, "stangrise.jpeg", 15, "Femme" , 37, "Grise"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (11, "stangrise.jpeg", 15, "Homme" , 46, "Grise"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (12, "stangrise.jpeg", 15, "Homme" , 46, "Grise"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (13, "sandroorange.jpeg", 16, "Femme" , 36, "Orange"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (14, "sandroorange.jpeg", 16, "Homme" , 42, "Orange"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (15, "sandroorange.jpeg", 16, "Femme" , 38, "Orange"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (16, "sandroorange.jpeg", 16, "Homme" , 44, "Orange"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (17, "sandronoire.jpeg", 16, "Homme" , 44, "Noire"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (18, "sandronoire.jpeg", 16, "Femme" , 36, "Noire"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (19, "sandronoire.jpeg", 16, "Homme" , 44, "Noire"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (20, "sacrouge.jpeg", 17, "Femme" , 0 , "Rouge"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (21, "sacrouge.jpeg", 17, "Femme" , 0, "Rouge"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (22, "sacrouge.jpeg", 17, "Femme" , 0,  "Rouge"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (23, "sacjaune.jpeg", 17, "Femme" , 0 , "Jaune"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (24, "sacjaune.jpeg", 17, "Femme" , 0 , "Jaune"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (25, "sacjaune.jpeg", 17, "Femme" , 0 , "Jaune"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (26, "sacjaune.jpeg", 17, "Femme" , 0 , "Jaune"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (27, "collierbleu.jpeg", 18, "Femme" , 0 , "Bleu"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (28, "collierbleu.jpeg", 18, "Femme" , 0 , "Bleu"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (29, "collierbleu.jpeg", 18, "Femme" , 0 , "Bleu"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (30, "collierbleu.jpeg", 18, "Femme" , 0 , "Bleu"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (31, "collierbleu.jpeg", 18, "Femme" , 0 , "Bleu"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (32, "collierrose.jpeg", 18, "Femme" , 0 , "Rose"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (33, "collierrose.jpeg", 18, "Femme" , 0 , "Rose"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (34, "collierrose.jpeg", 18, "Femme" , 0 , "Rose"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (35, "echarpenoire.jpeg", 19, "Femme" , 0 , "Noir"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (36, "echarpenoire.jpeg", 19, "Homme" , 0 , "Noir"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (37, "echarpeecoose.jpeg", 19, "Femme" , 0 , "Ecosse"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (38, "echarpeecoose.jpeg", 19, "Homme" , 0 , "Ecosse"); 
+
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (39, "echarpejaune.jpeg", 19, "Femme" , 0 , "Jaune"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (40, "echarpejaune.jpeg", 19, "Femme" , 0 , "Jaune"); 
+Insert into ArticleVetement (ID, Photo, IDType, Modele, Taille, Couleur) values (41, "echarpejaune.jpeg", 19, "Homme" , 0 , "Ecosse"); 
