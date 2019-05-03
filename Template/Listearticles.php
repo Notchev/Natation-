47cad8f9828dff931ef923d05f741fc0c84caa62
Sarah Kardache <sarahkardache@MacBook-Pro-de-Sarah.local>
2019-05-03 11:37:23 +0200

    linkage

--- Template/Listearticles.php
+++ Template/Listearticles.php
@@ -37,7 +37,7 @@ $db_found = mysqli_select_db($db_handle, $database);
   <!-- Navigation -->
   <nav class="navbar  navbar-expand-lg navbar-dark bg-primary fixed-top">
     <div class="container">
-      <a class="navbar-brand amazon1" href="#"> Amazon ECE  
+      <a class="navbar-brand amazon1" href="index.php"> Amazon ECE  
         <img src="Logo_ECE_Paris.png" width="110" height="30"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
